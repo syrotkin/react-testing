b2500da8768e66358138a3d59fc49e87b3f0f181
Oleksiy Syrotkin <oleksiysyrotkin@bbv.ch>
2020-09-13 20:25:56 +0200

    End of Lesson 14

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
 
 export default class MovieForm extends Component {
   state = {
@@ -11,19 +11,21 @@ export default class MovieForm extends Component {
     const { text } = this.state;
 
     return (
-      <div>
-        <form
-          data-testid="movie-form"
-          onSubmit={() => submitForm({ text })}
-        >
-          <input type="text" />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <form data-testid="movie-form" onSubmit={() => submitForm({ text })}>
+        {/* creating a controlled component */}
+        <label htmlFor="text">
+          Text
+          <input
+            type="text"
+            id="text"
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
-
 
 MovieForm.propTypes = {
   submitForm: PropTypes.func.isRequired,
--- src/MovieForm.test.js
+++ src/MovieForm.test.js
@@ -8,13 +8,25 @@ const onSubmit = jest.fn();
 
 test('<MovieForm />', () => {
   const {
-    queryByTestId, getByText, getByTestId
+    queryByTestId, getByText, getByTestId, getByLabelText
   } = render(<MovieForm submitForm={onSubmit} />);
 
   // 'movie-form' is in MovieForm itself
   expect(queryByTestId('movie-form')).toBeTruthy();
 
+  // Note: old syntax
+  const label = getByLabelText('Text');
+  label.value = 'hi';
+  fireEvent.change(label);
+
+  // Note: new syntax:
+  fireEvent.change(getByLabelText('Text'), { 
+    target: { value: 'hi' }
+  });
+
   // relying on the text 'Submit' to not change
   fireEvent.click(getByText('Submit'));
+
   expect(onSubmit).toHaveBeenCalledTimes(1);
+  expect(onSubmit).toHaveBeenCalledWith({ text: 'hi' });
 });
